974db11b397b0a81eb9ef080499fcd5e78a17bcb
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-09-13 21:33:55 +0200

    add Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Searchbar from 'components/Searchbar/Searchbar';
+
+class App extends Component {
+  state = {
+    searchRequest: '',
+  };
+
+  formSubmitHandler = searchRequest => {
+    console.log(this.state);
+    return this.setState(searchRequest);
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Searchbar onSubmit={this.formSubmitHandler} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  state = {
+    searchRequest: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ searchRequest: value });
+  };
+
+  сreateRequest = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      searchRequest: '',
+    });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.сreateRequest}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.searchRequest}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
